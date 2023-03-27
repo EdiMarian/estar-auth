f0985791ea41bd2e7944717511980ee8e1db42b4
EdiMarian <ichimedimarian@gmail.com>
2023-03-27 16:26:42 +0200

    check secret_token add

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { PrismaService } from '../prisma/prisma.service';
@@ -9,23 +9,27 @@ export class AuthService {
     constructor(private prismaService: PrismaService, private jwtService: JwtService, private configService: ConfigService) {}
 
     async login(dto: AuthDto): Promise<any> {
+        // check secret token if is correct
+        const secret_token = this.configService.get("SECRET_TOKEN");
+
+        if(secret_token !== dto.secret) throw new UnauthorizedException();
+
         // check user exists
         const userExists = await this.prismaService.user.findUnique({
             where: {
                 email: dto.email
             }
         });
-        if(!userExists) {
-            const user = await this.prismaService.user.create({
-                data: {
-                    email: dto.email,
-                    username: dto.username
-                }
-            });
-            delete user.id;
-            return user;
-        }
-        return userExists;
+        if(userExists) return this.signToken(userExists.id, userExists.email, userExists.username);
+
+        // create user
+        const user = await this.prismaService.user.create({
+            data: {
+                email: dto.email,
+                username: dto.username
+            }
+        });
+        return this.signToken(user.id, user.email, user.username);
     }
 
     signToken(userId: string, email: string, username: string): Promise<string> {
---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ConfigService } from '@nestjs/config';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(config: ConfigService) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: config.get('JWT_SECRET'),
+        })
+    }
+}
