d22553c265c12dcd68066381c5787fc0506f2510
EdiMarian <ichimedimarian@gmail.com>
2023-03-27 15:32:53 +0200

    login function logic add

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
     constructor(private prismaService: PrismaService) {}
 
-    async login(): Promise<string> {
+    async login(dto: AuthDto): Promise<any> {
+        // check user exists
+        const userExists = await this.prismaService.user.findUnique({
+            where: {
+                email: dto.email
+            }
+        });
+        if(!userExists) {
+            const user = await this.prismaService.user.create({
+                data: {
+                    email: dto.email,
+                    username: dto.username
+                }
+            });
+            delete user.id;
+            return user;
+        }
         return '';
     }
 }
