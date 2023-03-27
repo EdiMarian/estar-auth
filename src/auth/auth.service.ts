fb631b2b364530ee7fd98bcb5ae0b90044182d51
EdiMarian <ichimedimarian@gmail.com>
2023-03-27 21:15:50 +0200

    google-auth-library and logic add

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,35 +1,69 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException, ForbiddenException, NotFoundException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { PrismaService } from '../prisma/prisma.service';
-import { AuthDto } from './dto';
+import { RegisterDto } from './dto';
+import { OAuth2Client } from 'google-auth-library';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { LoginDto } from './dto/Login.dto';
+import { TokenPayload } from 'google-auth-library/build/src/auth/loginticket';
+
+const googleClient = new OAuth2Client(process.env.GOOGLE_CLIENT_ID)
 
 @Injectable()
 export class AuthService {
     constructor(private prismaService: PrismaService, private jwtService: JwtService, private configService: ConfigService) {}
 
-    async login(dto: AuthDto): Promise<any> {
-        // check secret token if is correct
-        const secret_token = this.configService.get("SECRET_TOKEN");
+    async register(dto: RegisterDto): Promise<any> {
+        try {
+            // check google token
+            const { email } = await this.verifyGoogleToken(dto.token);
 
-        if(secret_token !== dto.secret) throw new UnauthorizedException();
-
-        // check user exists
-        const userExists = await this.prismaService.user.findUnique({
-            where: {
-                email: dto.email
+            // create user
+            const user = await this.prismaService.user.create({
+                data: {
+                    email: email,
+                    username: dto.username
+                }
+            });
+            return this.signToken(user.id, user.email, user.username);
+            } catch (err) {
+                if(err instanceof PrismaClientKnownRequestError) {
+                    if(err.code === 'P2002') throw new ForbiddenException("User exists!")
+                }
+                throw err;
             }
-        });
-        if(userExists) return this.signToken(userExists.id, userExists.email, userExists.username);
+    }
 
-        // create user
-        const user = await this.prismaService.user.create({
-            data: {
-                email: dto.email,
-                username: dto.username
+    async login(dto: LoginDto): Promise<string> {
+        try {
+            // check google token
+            const { email } = await this.verifyGoogleToken(dto.token);
+
+            // check user exists
+            const user = await this.prismaService.user.findUnique({
+                where: {
+                    email: email
+                }
+            });
+            if(!user) throw new NotFoundException('User not found!');
+
+            return this.signToken(user.id, user.email, user.username);
+            } catch (err) {
+                throw new UnauthorizedException(err.message);
             }
-        });
-        return this.signToken(user.id, user.email, user.username);
+    }
+
+    async verifyGoogleToken(token: string): Promise<TokenPayload> {
+        try {
+            const google_token = await googleClient.verifyIdToken({
+                idToken: token,
+                audience: this.configService.get("GOOGLE_CLIENT_ID"),
+            });
+            return google_token.getPayload();
+        } catch (err) {
+            throw new UnauthorizedException(err.message);
+        }
     }
 
     signToken(userId: string, email: string, username: string): Promise<string> {
