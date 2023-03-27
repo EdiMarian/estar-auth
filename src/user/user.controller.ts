48785617854989a062d6193aa8f49895e31bbe02
EdiMarian <ichimedimarian@gmail.com>
2023-03-27 16:47:16 +0200

    user decorator add

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, UseGuards, Get } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { JwtGuard } from '../auth/guard';
+import { GetUser } from './decorator';
 
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
 
     @Get('')
-    getUsers() {
-        return "hi";
+    getUsers(@GetUser() user: User) {
+        return user;
     }
 }
