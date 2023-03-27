151a0d9438213edb9c47bde7048867fe303e8ae6
EdiMarian <ichimedimarian@gmail.com>
2023-03-27 15:24:59 +0200

    authDto init & global pipes added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthDto } from './dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+    constructor(private authService: AuthService) {}
     @Post("/login")
-    login() {
-        
+    login(@Body() dto: AuthDto) {
+        return this.authService.login();
     }
 }
