3ec5fe6d389f9eea861d37bb332d85a03ffa1f23
EdiMarian <ichimedimarian@gmail.com>
2023-03-27 17:24:34 +0200

    jwt strategy & getUser decorator updated

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -2,17 +2,24 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ConfigService } from '@nestjs/config';
 import { Strategy, ExtractJwt } from 'passport-jwt';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-    constructor(config: ConfigService) {
+    constructor(config: ConfigService, private prismaService: PrismaService) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             secretOrKey: config.get('JWT_SECRET'),
         })
     }
     
-    validate(payload: any) {
-        return payload;
+    async validate(payload: any) {
+        const user = await this.prismaService.user.findUnique({
+            where: {
+                id: payload.sub,
+            }
+        });
+        delete user.id;
+        return user;
     }
 }
