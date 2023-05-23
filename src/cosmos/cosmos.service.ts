f943f4c7e78a5ca4c3116e9329fcc571aaed2fba
EdiMarian <ichimedimarian@gmail.com>
2023-05-23 11:08:22 +0200

    cosmosdb connect done

--- src/cosmos/cosmos.service.ts
+++ src/cosmos/cosmos.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Container, CosmosClient, Database } from '@azure/cosmos';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class CosmosService {}
+export class CosmosService {
+    database: Database;
+    constructor(private readonly configService: ConfigService) {
+        const client = new CosmosClient({
+            endpoint: this.configService.get('COSMOS_ENDPOINT'),
+            key: this.configService.get('COSMOS_KEY'),
+        })
+        this.database = client.database(this.configService.get('COSMOS_DATABASE'));
+    }
+
+    users(): Container {
+        return this.database.container('users');
+    }
+}
