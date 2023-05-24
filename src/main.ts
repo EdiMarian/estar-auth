870aaf80e5876d63874eb7b17c8544be12ddebee
EdiMarian <ichimedimarian@gmail.com>
2023-05-24 13:03:26 +0200

    port add in .env

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3142);
+  await app.listen(app.get(ConfigService).get('NEST_JS_PORT'));
 }
 bootstrap();
