e67705de198525ed7464a0ffa14d6d5815cc4460
EdiMarian <ichimedimarian@gmail.com>
2023-05-29 13:12:52 +0200

    admin init

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { CosmosModule } from './cosmos/cosmos.module';
 import { ShopModule } from './shop/shop.module';
+import { AdminModule } from './admin/admin.module';
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true}), AuthModule, UserModule, CosmosModule, ShopModule],
+  imports: [ConfigModule.forRoot({isGlobal: true}), AuthModule, UserModule, CosmosModule, ShopModule, AdminModule],
   controllers: [],
   providers: [],
 })
