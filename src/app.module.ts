212bb56e91fff8c98949cf7fc72879eeca57d03d
EdiMarian <ichimedimarian@gmail.com>
2023-05-29 13:01:07 +0200

    shop init

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { CosmosModule } from './cosmos/cosmos.module';
+import { ShopModule } from './shop/shop.module';
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true}), AuthModule, UserModule, CosmosModule],
+  imports: [ConfigModule.forRoot({isGlobal: true}), AuthModule, UserModule, CosmosModule, ShopModule],
   controllers: [],
   providers: [],
 })
