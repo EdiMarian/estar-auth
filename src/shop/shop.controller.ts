ffff42e1d5460135226e277490f8cfa9a6196eb5
EdiMarian <ichimedimarian@gmail.com>
2023-05-29 13:39:16 +0200

    admin-guard added in shop-controller

--- src/shop/shop.controller.ts
+++ src/shop/shop.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, UseGuards, Get } from '@nestjs/common';
+import { Controller, UseGuards, Post, Get } from '@nestjs/common';
 import { AdminGuard } from 'src/admin/guard/admin.guard';
 
-@UseGuards(AdminGuard)
 @Controller('shop')
 export class ShopController {
 
+    @Post('/item')
+    @UseGuards(AdminGuard)
+    createItem() {}
+
     @Get('/items')
     getItems() {
-        return 'salut'
+        return ''
     }
 }
