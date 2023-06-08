4578cf32dc53513f6ed407ff2a644c140644bc4c
EdiMarian <ichimedimarian@gmail.com>
2023-06-08 13:59:51 +0200

    const level add

--- src/common/constants/index.ts
+++ src/common/constants/index.ts
@@ -1,1 +1,2 @@
 export * from './list.of.negative.words'
+export * from './levels'
---
+++ src/common/constants/levels.ts
@@ -0,0 +1,30 @@
+export const levels = [
+    {
+        level: 1,
+        xp: 3400
+    },
+    {
+        level: 2,
+        xp: 11900
+    },
+    {
+        level: 3,
+        xp: 25500
+    },
+    {
+        level: 4,
+        xp: 59500
+    },
+    {
+        level: 5,
+        xp: 127500
+    },
+    {
+        level: 6,
+        xp: 255000
+    },
+    {
+        level: 7,
+        xp: 680000
+    },
+]
