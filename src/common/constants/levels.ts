95703573eb155661d8bc13a5f5fdc6bab1a1cb93
EdiMarian <ichimedimarian@gmail.com>
2023-06-08 14:22:30 +0200

    update levels

--- src/common/constants/levels.ts
+++ src/common/constants/levels.ts
@@ -1,30 +1,34 @@
 export const levels = [
     {
         level: 1,
-        xp: 3400
+        requiredXP: 3400
     },
     {
         level: 2,
-        xp: 11900
+        requiredXP: 11900
     },
     {
         level: 3,
-        xp: 25500
+        requiredXP: 25500
     },
     {
         level: 4,
-        xp: 59500
+        requiredXP: 59500
     },
     {
         level: 5,
-        xp: 127500
+        requiredXP: 127500
     },
     {
         level: 6,
-        xp: 255000
+        requiredXP: 255000
     },
     {
         level: 7,
-        xp: 680000
+        requiredXP: 680000
     },
+    {
+        level: 8,
+        requiredXP: Infinity
+    }
 ]
